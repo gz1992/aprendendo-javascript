b2bfda9d36e22ed273f07fff380b7ef33ba553fa
Germano <germano.zaicaner@smarthis.com.br>
2021-02-04 18:16:07 -0300

    adicionando basicojs

---
+++ basico.js
@@ -0,0 +1,29 @@
+// Conceitos básicos
+
+
+// Declarando variáveis:
+let x = 0;
+var y = 'hello world';
+const z = true;
+
+// Estruturas condicionais:
+if (x > 0) {
+    console.log('X é maior que zero');
+}
+else {
+    console.log('X não é maior que zero');
+}
+
+//Funções
+function quadrado(a) {
+    return a * a;
+}
+quadrado(10) // retorna 100
+// Função anônima
+quadradoFuncaoAnonimo = (a) => {
+    return a * a;
+}
+quadradoFuncaoAnonimo(20) // retorna 400
+
+
+// PERGUNTAS:
