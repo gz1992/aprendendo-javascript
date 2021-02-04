2cc0b0b884889c625144cdbe08f1a4bc8f111791
Germano <germano.zaicaner@smarthis.com.br>
2021-02-04 18:26:31 -0300

    adicinoando arrays e objetos + dicionario

--- basico.js
+++ basico.js
@@ -1,11 +1,19 @@
-// Conceitos básicos
-
-
 // Declarando variáveis:
 let x = 0;
 var y = 'hello world';
 const z = true;
+// Funcionamento array
+let meu_array = [10, y, 'option'];
+// acessando posições do array
+meu_array[1] // retorna 'hello world'
+// Funcionamento objetos
+let pessoa = { nome:'João Goulart', idade:55 };
+pessoa['nome'] // retorna 'João Goulart'
+pessoa.nome // retorna 'João Goulart'
+var chave = 'nome';
+pessoa[chave] // retorna 'João Goulart'
 
+/* ************************************************************************************ */
 // Estruturas condicionais:
 if (x > 0) {
     console.log('X é maior que zero');
@@ -14,6 +22,7 @@ else {
     console.log('X não é maior que zero');
 }
 
+/* ************************************************************************************ */
 //Funções
 function quadrado(a) {
     return a * a;
@@ -25,5 +34,5 @@ quadradoFuncaoAnonimo = (a) => {
 }
 quadradoFuncaoAnonimo(20) // retorna 400
 
-
+/* ************************************************************************************ */
 // PERGUNTAS:
